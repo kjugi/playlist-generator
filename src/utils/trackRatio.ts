0c019c58cbaa5d9edede626c531792233b2658c1
Filip Kudla <filipkud31@gmail.com>
2022-06-14 00:35:13 +0200

    Add TODO to trackRatio util

--- src/utils/trackRatio.ts
+++ src/utils/trackRatio.ts
@@ -17,5 +17,8 @@ export const trackRatio = (
   const percentagePerAlbum = albumsPopularity.map(singlePopularity => newPercentageTotal * singlePopularity);
   const songsPerAlbum = percentagePerAlbum.map(value => Math.round(songsPerArtist * (value / 100)));
 
+  // TODO: Check is any album has more than 0 sons per album
+  // Happens when artist has a lot of albums and songsPerArtist is low.
+  // Let's run in the loop: cut the last album (less popular) and check for songsPerArtist count again
   return songsPerAlbum
 };
